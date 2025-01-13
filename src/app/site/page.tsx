39b904a260c268a41e8e9a50223e7c2a921776c6
Lucas Nakeli <luminouslucas@duck.com>
2025-01-13 11:38:47 +0200

    Refactor Home component layout and styles in page.tsx. Updated section structure, added a tagline, and improved gradient backgrounds for better visual appeal.

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,20 +1,24 @@
 export default async function Home() {
   return (
-    <main className="">
+    <>
       <section
         className="h-full w-full pt-36 relative flex 
-        items-center justify-center flex-col bg-black"
+        items-center justify-center flex-col "
       >
-        <div className="relative h-full w-full bg-white">
-          <div
-            className="absolute bottom-0 left-0 right-0 top-0 
-            bg-[linear-gradient(to_right,#161616_1px,transparent_1px),
-            linear-gradient(to_bottom,#161616_1px,transparent_1px)] 
-            bg-[size:4rem_4rem] [mask-image:radial-gradient(
-            ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]"
-          ></div>
+        {}
+          <div className="absolute bottom-0 left-0 right-0 top-0 
+          bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] 
+          bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+        <p className="text-center">Run your agency from one place</p>
+        <div
+          className="bg-gradient-to-r from-primary to-secondary-foreground 
+        text-transparent bg-clip-text relative"
+        >
+          <h1 className="text-9xl font-bold text-center md:text-[300px]">
+            Plura
+          </h1>
         </div>
       </section>
-    </main>
+    </>
   );
 }
