4afce4e20036d08f005f32afc36bd52cf0868e46
Lucas Nakeli <luminouslucas@duck.com>
2025-01-13 13:17:46 +0200

    Enhance Home component in page.tsx by adding an image and refining layout. Imported Image component from Next.js, adjusted div structure for better styling, and included a new gradient overlay for improved visual hierarchy.

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default async function Home() {
   return (
     <>
@@ -6,9 +8,11 @@ export default async function Home() {
         items-center justify-center flex-col "
       >
         {}
-          <div className="absolute bottom-0 left-0 right-0 top-0 
+        <div
+          className="absolute bottom-0 left-0 right-0 top-0 
           bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] 
-          bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+          bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]"
+        ></div>
         <p className="text-center">Run your agency from one place</p>
         <div
           className="bg-gradient-to-r from-primary to-secondary-foreground 
@@ -18,7 +22,22 @@ export default async function Home() {
             Plura
           </h1>
         </div>
+        <div
+          className="flex justify-center items-center relative
+        md:mt-[-70px]"
+        >
+          <Image
+            src={"/assets/preview.png"}
+            alt="The banner image"
+            height={1200}
+            width={1200}
+            className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
+          />
+        </div>
       </section>
     </>
   );
 }
+
+<div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background 
+left-0 right-0 absolute z-10"></div>
